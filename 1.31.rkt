e8993aefbaaa6b884cf80cd91359b942595fa773
Edward Falcon <devedwardfalcon@gmail.com>
2020-11-11 22:41:46 +0300

    add exercises 1.30, 1.31

---
+++ 1.31.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(define (inc x) (+ x 1))
+
+(define (identity x) x)
+
+(define (product term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (* result (term a)))))
+  (iter a 1))
+
+(define (factorial n)
+  (product identity 1 inc n))
